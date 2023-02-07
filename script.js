69eae69ed86ffa83413be4d4d45dbca5a9106683
iain macdonald <loki@mail.com>
2023-02-07 17:48:52 +0000

    tweaked the api

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date")
+
+
 const apiKey = "08e41f3988dcc8f9767ada1d7e0a7571";
 const getCity = document.querySelector(".city"); 
 const temperature = document.querySelector(".temp");
-const ic = document.querySelector(".icon");
+const ico = document.querySelector(".icon");
 const desc = document.querySelector(".description");
-const hum = document.querySelector(".humidity");
+const humi = document.querySelector(".humidity");
 const win = document.querySelector(".wind");
-let weather_result = {
-    fetch_weather: function(city) {
-        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
-    }
-}
+
+let weather_result = fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + 'madrid' + "&appid=" + apiKey);
+weather_result.then(function (response) {
+    return response.json()
+}).then(function(data) {
+    console.log(data);
+    const lat = data[0].lat
+    const lon = data[0].lon
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat +"&lon=" + lon +"&units=metric&appid=" + apiKey).then(function(response){
+        return response.json()
+    }).then(function(data) {
+        console.log(data);
+    })
+})
 
 
 
